2c168de703c02b5aa6af5f9fc7955b434591a07c
kasreder <kasreder@naver.com>
2025-11-01 22:27:40 +0900

    feat: 데이터베이스 자동 초기화 구현

---
+++ backend/src/utils/logger.ts
@@ -0,0 +1,21 @@
+import { createLogger, format, transports } from 'winston';
+
+const { combine, colorize, timestamp, printf, splat, align, errors } = format;
+
+const consoleFormat = printf(({ level, message, timestamp: time, stack, ...meta }) => {
+  const metaString = Object.keys(meta).length > 0 ? ` ${JSON.stringify(meta)}` : '';
+  const logMessage = stack ?? message;
+  return `${time} [${level}]: ${logMessage}${metaString}`;
+});
+
+const logger = createLogger({
+  level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+  format: combine(errors({ stack: true }), splat(), timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), align()),
+  transports: [
+    new transports.Console({
+      format: combine(colorize(), consoleFormat),
+    }),
+  ],
+});
+
+export default logger;
